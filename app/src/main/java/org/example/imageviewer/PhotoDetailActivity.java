c42cfa7141d57a6723687aae851dd4e5c4e825de
Bipul1895 <bipuliitian2016@gmail.com>
2020-07-14 13:42:41 +0530

    PhotoDetailActivity working, all basic functions implemented

--- app/src/main/java/org/example/imageviewer/PhotoDetailActivity.java
+++ app/src/main/java/org/example/imageviewer/PhotoDetailActivity.java
@@ -1,11 +1,11 @@
 package org.example.imageviewer;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class PhotoDetailActivity extends BaseActivity {
 
@@ -15,6 +15,27 @@ public class PhotoDetailActivity extends BaseActivity {
         setContentView(R.layout.activity_photo_detail);
 
         activateToolbar(true);
+
+        Intent intent = getIntent();
+        Photo photoData = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
+
+        if(photoData != null){
+            TextView photoTitle = findViewById(R.id.photo_title);
+            photoTitle.setText("Title: " + photoData.getTitle());
+
+            TextView photoTags = findViewById(R.id.photo_tags);
+            photoTags.setText("Tags: " + photoData.getTags());
+
+            TextView photoAuthor = findViewById(R.id.photo_author);
+            photoAuthor.setText(photoData.getAuthor());
+
+            ImageView photoImage = findViewById(R.id.photo_image);
+
+            Picasso.get().load(photoData.getLink())
+                    .error(R.drawable.broken_image_placeholder)
+                    .placeholder(R.drawable.placeholder)
+                    .into(photoImage);
+        }
 
     }
 
